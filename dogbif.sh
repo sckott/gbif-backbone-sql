75a88272170f30f0c2acf6aae981919d70468609
Scott Chamberlain <myrmecocystus@gmail.com>
2019-07-31 16:37:17 -0700

    remove quotes in Taxon.tsv

--- dogbif.sh
+++ dogbif.sh
@@ -1,4 +1,6 @@
 sqlite3 gbif.sqlite < schema.sql
+echo "removing any quotes in Taxon.tsv"
+sed -i 's/\"//g' Taxon.tsv
 echo '.mode tabs\n.separator "\\t"\n.header off\n.import Taxon.tsv gbif' | sqlite3 gbif.sqlite
 echo "CREATE UNIQUE INDEX id on gbif (taxonID);" | sqlite3 gbif.sqlite
 echo "\nall done!"
