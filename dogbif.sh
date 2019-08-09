cc7bb98c6da1a33c4f229dfab44fac6e1cf11f22
Scott Chamberlain <myrmecocystus@gmail.com>
2019-08-09 10:54:59 -0700

    use linux version of sed command in dogbif.sh

--- dogbif.sh
+++ dogbif.sh
@@ -1,6 +1,6 @@
 echo "removing any quotes in Taxon.tsv"
 # sed -i '' 's/\"//g' Taxon.tsv
-sed -i '' 's/\"//g' Taxon.tsv
+sed -i 's/\"//g' Taxon.tsv
 sqlite3 gbif.sqlite < schema.sql
 echo "quotes removed; now loading database"
 echo '.mode tabs\n.separator "\\t"\n.header off\n.import Taxon.tsv gbif' | sqlite3 gbif.sqlite
