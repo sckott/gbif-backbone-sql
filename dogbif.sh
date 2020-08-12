f11d0dded7e171efd171d78fd11f76405120656d
Scott Chamberlain <myrmecocystus@gmail.com>
2020-08-12 16:01:07 -0700

    add additional index on scientificname

--- dogbif.sh
+++ dogbif.sh
@@ -6,5 +6,6 @@ echo "quotes removed; now loading database"
 echo '.mode tabs\n.separator "\\t"\n.header off\n.import Taxon.tsv gbif' | sqlite3 gbif.sqlite
 echo "CREATE UNIQUE INDEX id on gbif (taxonID);" | sqlite3 gbif.sqlite
 echo "CREATE INDEX conname on gbif (canonicalName);" | sqlite3 gbif.sqlite
+echo "CREATE INDEX sciname on gbif (scientificName);" | sqlite3 gbif.sqlite
 echo "CREATE INDEX parname on gbif (parentNameUsageID);" | sqlite3 gbif.sqlite
 echo "\nall done!"
