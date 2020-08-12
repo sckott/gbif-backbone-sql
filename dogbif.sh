3731956f61b610d9447b21bd1bad7262547b3eeb
Scott Chamberlain <myrmecocystus@gmail.com>
2020-08-11 18:29:10 -0700

    add indices on sci and canonical name cols

--- dogbif.sh
+++ dogbif.sh
@@ -5,4 +5,6 @@ sqlite3 gbif.sqlite < schema.sql
 echo "quotes removed; now loading database"
 echo '.mode tabs\n.separator "\\t"\n.header off\n.import Taxon.tsv gbif' | sqlite3 gbif.sqlite
 echo "CREATE UNIQUE INDEX id on gbif (taxonID);" | sqlite3 gbif.sqlite
+echo "CREATE INDEX sciname on gbif (scientificName);" | sqlite3 gbif.sqlite
+echo "CREATE INDEX conname on gbif (canonicalName);" | sqlite3 gbif.sqlite
 echo "\nall done!"
